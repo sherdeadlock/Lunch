e533886127533d71bc7d3ed2656e0b5817347442
nil <nil@nil>
2016-01-23 19:27:33 +0800

    sign debug apk

--- lunch/src/main/java/xxx/lunch/LunchAdapter.java
+++ lunch/src/main/java/xxx/lunch/LunchAdapter.java
@@ -2,7 +2,6 @@ package xxx.lunch;
 
 import android.content.Context;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
